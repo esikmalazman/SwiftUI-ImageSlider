831ff91127118a982a02639cfa530341f3ddfeed
Ikmal Azman <opikmalazman@gmail.com>
2023-06-12 21:17:08 +0100

    Initial Commit

---
+++ ImageSlider-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImageSlider-SwiftUI
+//
+//  Created by Ikmal Azman on 12/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageSlider-SwiftUI/ImageSlider_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSlider_SwiftUIApp.swift
+//  ImageSlider-SwiftUI
+//
+//  Created by Ikmal Azman on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageSlider_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
