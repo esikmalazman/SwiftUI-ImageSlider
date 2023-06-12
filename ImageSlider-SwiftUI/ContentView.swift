8c79902cb877b2ec919131d28a0c824ec1974fde
Ikmal Azman <opikmalazman@gmail.com>
2023-06-12 22:23:10 +0100

    implement image slider in contentView

--- ImageSlider-SwiftUI/ContentView.swift
+++ ImageSlider-SwiftUI/ContentView.swift
@@ -3,19 +3,24 @@
 //  ImageSlider-SwiftUI
 //
 //  Created by Ikmal Azman on 12/06/2023.
-//
+//  Image slider in SwiftUI
+//  https://medium.com/@mahdi.mahjoobi/image-slider-in-swiftui-59bac18ae4f4
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            /// 1. create List view and emded the image slider with specified height
+            List {
+                ImageSlider()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets())
+            }
+            .listStyle(.plain)
+            .navigationBarTitleDisplayMode(.large)
+            .navigationBarTitle("Image Slider")
         }
-        .padding()
     }
 }
 
